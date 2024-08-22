6e172056cc7c6faed41ed4f41a514020c7c049f7
Moustafa Elsonbaty <elsonbaty@gmail.com>
2024-08-22 09:24:32 +0400

    new

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .models import Task
 
 # Create your views here.
 def addtask(request):
     task = request.POST['task']
     Task.objects.create(task=task)
-    return HttpResponse('Form submitted successfully')
+    return redirect('home')
