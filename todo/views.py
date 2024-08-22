f9778582829b41d6165a0e5a2b58b0dfd2438468
Moustafa Elsonbaty <elsonbaty@gmail.com>
2024-08-22 08:47:29 +0400

    new file:   todo/urls.py

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Task
 
 # Create your views here.
+def addtask(request):
+    task = request.POST['task']
+    Task.objects.create(task=task)
+    return HttpResponse('Form submitted successfully')
